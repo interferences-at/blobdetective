22d29a3988c521ef3c11f1841d733eb7fc0df186
Alexandre Quessy <alexandre@quessy.net>
2016-03-17 23:35:07 -0400

    add methods to the Configuration class

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -7,4 +7,45 @@ Configuration::Configuration()
     this->video_device_id = 0;
 }
 
+bool Configuration::has_option(const char* name) const
+{
+    std::map<std::string, Option>::const_iterator iter = this->_options.find(std::string(name));
+    if (iter == this->_options.end())
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+Option* Configuration::get_option(const char *name)
+{
+    std::map<std::string, Option>::iterator iter = this->_options.find(std::string(name));
+    if (iter == this->_options.end())
+    {
+        return NULL;
+    }
+    else
+    {
+        return & (iter->second);
+    }
+}
+
+void Configuration::add_option(const char* name, Option option)
+{
+    this->_options.insert(std::map<std::string, Option>::value_type(std::string(name), option));
+}
+
+const std::vector<std::string> Configuration::list_options() const
+{
+    std::vector<std::string> ret;
+    for (std::map<std::string, Option>::const_iterator item = this->_options.begin(); item != this->_options.end(); ++item)
+    {
+        ret.push_back(item->second.get_name());
+    }
+    return ret;
+}
+
 } // end of namespace
--- src/configuration.h
+++ src/configuration.h
@@ -1,6 +1,11 @@
 #ifndef __CONFIGURATION_H__
 #define __CONFIGURATION_H__
 
+#include <string>
+#include <map>
+#include <vector>
+#include "option.h"
+
 namespace blobdetective {
 
 struct Configuration
@@ -8,6 +13,12 @@ struct Configuration
     public:
         Configuration();
         int video_device_id;
+        bool has_option(const char* name) const;
+        Option* get_option(const char *name);
+        const std::vector<std::string> list_options() const;
+        void add_option(const char* name, Option option);
+    private:
+        std::map<std::string, Option> _options;
 };
 
 } // end of namespace
