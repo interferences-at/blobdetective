8ac76c3a5b91c00493c1ac30560f5a509d8cf47b
Alexandre Quessy <alexandre@quessy.net>
2016-04-30 19:01:22 -0400

    add simple-1.sh and simple-0.sh

---
+++ scripts/simple-0.sh
@@ -0,0 +1,8 @@
+~/src/blobdetective/src/blobdetective \
+    -v \
+    -o video_device_id 0 \
+    -o identifier 0 \
+    -o osc_receive_port 31444 \
+    -o osc_send_host 127.0.0.1 \
+    -o osc_send_port 31340 \
+    -o verbose true
