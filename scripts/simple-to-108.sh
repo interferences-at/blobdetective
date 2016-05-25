7fbe995020922eab25885785fca7ca3690f5ccfc
Alexandre Quessy <alexandre@quessy.net>
2016-05-25 13:36:59 -0400

    add simple-to-108.sh

---
+++ scripts/simple-to-108.sh
@@ -0,0 +1,10 @@
+~/src/blobdetective/src/blobdetective \
+    -v \
+    -o video_device_id 1 \
+    -o identifier 0 \
+    -o osc_receive_port 31444 \
+    -o osc_send_host 192.168.1.108 \
+    -o osc_send_port 31340 \
+    -o video_width 160 \
+    -o video_height 120 \
+    -o verbose true
